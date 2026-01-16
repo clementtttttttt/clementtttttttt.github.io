e531e0ba609b9caa653d086b1058c3c283ba95c3
clement <clementtttttttt@gmail.com>
2026-01-16 01:56:01 +0000

    make portfolio html actualy show photos

--- include/header.hh
+++ include/header.hh
@@ -5,7 +5,7 @@
 		<div id="navbar-div">
 		<div id="navbar">
 			<a id="gamez" href="wasmgamez.html">WASM Gamez</a>
-			<a id="port" href="#" >Photography portfolio</a>
+			<a id="port" href="portfolio.html" >Photography portfolio</a>
 			<a id="elec" href="electronics.html">Electronics shenanigans</a>
 			<a id="abm" href="index.html">About me</a>
 		</div>
