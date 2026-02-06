6fb9cbf104c52bfbc986466dc8b43732df4158da
clement <clementtttttttt@gmail.com>
2026-02-06 21:35:49 +0000

    css revamp

--- include/header.hh
+++ include/header.hh
@@ -2,18 +2,16 @@
 
 <hgroup id="header">
 		<div id="title">
-				<h1>clementtttttttt's fabulous games and stuff</h1>
+				<p>clementtttttttt's fabulous games and stuff</p>
 		</div>
-		<nav id="navbar-div">
-			<div id="navbar">
+
+</hgroup>
+<nav id="navbar">
 				<a id="gamez" href="wasmgamez.html">WASM Gamez</a>
 				<a id="port" href="portfolio.html" >Photography portfolio</a>
 				<a id="elec" href="electronics.html">Electronics shenanigans</a>
 				<a id="abm" href="index.html">About me</a>
-			</div>
-		</nav>
-</hgroup>
-	
+</nav>
 	
 	
 #define MARK_ACTIVE(id) <script>document.getElementById(id).classList.add("active");</script>
