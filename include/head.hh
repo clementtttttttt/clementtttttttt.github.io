baba106244a2078d7bdf035b6d92f6bd3739867a
clement <clementtttttttt@gmail.com>
2026-02-06 20:44:34 +0000

    add head.hh and put everything in header.css

---
+++ include/head.hh
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<head>
+ <meta charset="UTF-8">
+
+ <link href="https://fonts.cdnfonts.com/css/nimbus-sans-l" rel="stylesheet">
+ <link href="https://fonts.cdnfonts.com/css/frutiger" rel="stylesheet">
+ <link href="https://fonts.cdnfonts.com/css/gill-sans-2" rel="stylesheet">
+ <link rel="stylesheet" href="index.css">
+
+ <title>clementtttttttt's wasm games and stuff</title>
+ 
+ <meta name="viewport" content="width=device-width,initial-scale=1.0">
+</head>
