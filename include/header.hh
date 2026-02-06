1ef89d73e133ddf76048555b3e5d15eca9210e24
clement <clementtttttttt@gmail.com>
2026-02-06 20:57:23 +0000

    fix no update

--- include/header.hh
+++ include/header.hh
@@ -1,16 +1,19 @@
-	<div id="header">
+<link rel="stylesheet" href="header.css">
+
+<hgroup id="header">
 		<div id="title">
 				<h1>clementtttttttt's fabulous games and stuff</h1>
 		</div>
-		<div id="navbar-div">
-		<div id="navbar">
-			<a id="gamez" href="wasmgamez.html">WASM Gamez</a>
-			<a id="port" href="portfolio.html" >Photography portfolio</a>
-			<a id="elec" href="electronics.html">Electronics shenanigans</a>
-			<a id="abm" href="index.html">About me</a>
-		</div>
-		</div>
-	</div>
+		<nav id="navbar-div">
+			<div id="navbar">
+				<a id="gamez" href="wasmgamez.html">WASM Gamez</a>
+				<a id="port" href="portfolio.html" >Photography portfolio</a>
+				<a id="elec" href="electronics.html">Electronics shenanigans</a>
+				<a id="abm" href="index.html">About me</a>
+			</div>
+		</nav>
+</hgroup>
+	
 	
 	
 #define MARK_ACTIVE(id) <script>document.getElementById(id).classList.add("active");</script>
