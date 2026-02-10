db101ca506e43bcb90bfb308b0e6825306d17a9d
ec25589 <ec25589@cln-tsl-ftjfsy3.qm.ds.qmul.ac.uk>
2026-02-10 12:44:16 +0000

    link electronics in navbar to eagle88

---
+++ include/elec_header.hh
@@ -0,0 +1,9 @@
+		MARK_ACTIVE("elec")
+<nav class="navbar">
+				<a id="ee88" href="/projects/eagle88.html" >Eagle-88 microcomputer</a>
+				<a id="eosp" href="/projects/osprey.html">OSPREY PCU</a>
+				<a id="erc" href="/projects/turborc.html">Turbo mod RC cars</a>
+</nav>
+	
+	
+
--- include/header.hh
+++ include/header.hh
@@ -6,10 +6,10 @@
 		</div>
 
 </header>
-<nav id="navbar">
+<nav class="navbar">
 				<a id="gamez" href="/wasmgamez.html">WASM Gamez</a>
 				<a id="port" href="/portfolio.html" >Photography portfolio</a>
-				<a id="elec" href="/electronics.html">Electronics shenanigans</a>
+				<a id="elec" href="/projects/eagle88.html">Electronics shenanigans</a>
 				<a id="abm" href="/index.html">About me</a>
 </nav>
 	
